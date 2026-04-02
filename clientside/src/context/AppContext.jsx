9bee45bee8fd48e3901adb4bcb7dedf280e6308a
Shakti Singh <shakti33123singh@gmail.com>
2026-04-02 17:26:15 +0530

    fixed backed url fallback

--- clientside/src/context/AppContext.jsx
+++ clientside/src/context/AppContext.jsx
@@ -6,7 +6,7 @@ export const AppContext = createContext();
 
 const AppContextProvider = (props) => {
   const currencySymbol = "$";
-  const backendUrl = import.meta.env.VITE_BACKEND_URL;
+  const backendUrl = import.meta.env.VITE_BACKEND_URL || "https://doctor-appointment-app-14a0.onrender.com";
 
   const [doctors, setDoctors] = useState([]);
   const [token, setToken] = useState(
